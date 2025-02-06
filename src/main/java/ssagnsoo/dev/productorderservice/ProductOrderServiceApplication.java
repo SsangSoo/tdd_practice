73f44f0eed7a8a8d063cb79623beeb6abee518be
SsangSoo <kss10080@naver.com>
2025-02-06 09:46:08 +0900

    init project

---
+++ src/main/java/ssagnsoo/dev/productorderservice/ProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package ssagnsoo.dev.productorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssagnsoo/dev/productorderservice/ProductOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ssagnsoo.dev.productorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
